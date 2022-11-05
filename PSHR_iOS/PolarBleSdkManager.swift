ece0f8e2020032cdee3513bf427ac0a72931311f
Lawreros <lawreros@gmail.com>
2022-11-05 12:31:43 -0400

    create layout for app and include necessary image assets

---
+++ PSHR_iOS/PolarBleSdkManager.swift
@@ -0,0 +1,8 @@
+//
+//  PolarBleSdkManager.swift
+//  PSHR_iOS
+//
+//  Created by Ross on 11/5/22.
+//
+
+import Foundation
---
+++ PSHR_iOS/Views/UiHelper.swift
@@ -0,0 +1,8 @@
+//
+//  UiHelper.swift
+//  PSHR_iOS
+//
+//  Created by Ross on 11/5/22.
+//
+
+import Foundation
