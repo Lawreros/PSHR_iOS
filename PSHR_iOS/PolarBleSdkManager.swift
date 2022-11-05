2c3459371451cc7376afbc2c6c79691b3dff5b7e
Lawreros <lawreros@gmail.com>
2022-11-05 13:48:38 -0400

    construction of app with comments

--- PSHR_iOS/PolarBleSdkManager.swift
+++ PSHR_iOS/PolarBleSdkManager.swift
@@ -6,3 +6,115 @@
 //
 
 import Foundation
+import PolarBleSdk
+import RxSwift
+import CoreBluetooth
+import AudioToolbox
+import AVFoundation
+
+
+class PolarBleSdkManager : ObservableObject {
+    
+    private var api = PolarBleApiDefaultImpl.polarImplementation(DispatchQueue.main, features: Features.allFeatures.rawValue)
+    
+    // Create placeholder deviceID which will be changed when a real
+    // polar strap device is connected
+    private var deviceID = "00000000"
+    
+    // Strings which will publish updates on the hr/ecg/battery info
+    // These are what ContentView references
+    @Published public var hr_message = ""
+    @Published public var ecg_message = ""
+    @Published public var battery_level = ""
+    
+    // Variables which hold the status for BLE connection/searching
+    @Published private(set) var isBluetoothOn: Bool
+    @Published private(set) var isBroadcastListenOn: Bool = false
+    @Published private(set) var isSearchOn: Bool = false
+    
+    
+    private var autoConnectDisposable: Disposable?
+    
+    
+    
+    //Stores the state of the device being connected
+    @Published private(set) var deviceConnectionState: ConnectionState =
+        ConnectionState.disconnected {
+        didSet {//didset executes whenever the value is set
+            switch deviceConnectionState {
+            case .disconnected:
+                isDeviceConnected = false
+            case .connecting(_):
+                isDeviceConnected = false
+            case .connected(_):
+                isDeviceConnected = true
+            }
+        }
+    }
+    
+    @Published private(set) var isDeviceConnected: Bool = false
+    
+    
+    init() {
+        //check on initalization that Bluetooth is on
+        self.isBluetoothOn = api.isBlePowered
+        
+        api.polarFilter(true)
+        api.observer = self
+        api.deviceFeaturesObserver = self
+        api.powerStateObserver = self
+        api.deviceInfoObserver = self
+        api.sdkModeFeatureObserver = self
+        api.deviceHrObserver = self
+        api.logger = self
+    }
+    
+    // func broadcastToggle: searches for ble devices which are broadcasting
+    // func searchToggle: searches for polar devices
+    
+    
+    func connectToDevice() {
+        do {
+            try api.connectToDevice(deviceId)
+        } catch let err {
+            NSLog("Failed to connect to \(deviceId). Reason \(err)")
+        }
+    }
+    
+    func disconnectFromDevice() {
+        if case .connected(let deviceId) = deviceConnectionState {
+            do {
+                try api.disconnectFromDevice(deviceId)
+            } catch let err {
+                NSLog("Failed to disconnect from \(deviceId). Reason \(err)")
+            }
+        }
+    }
+    
+    //function to autoconnect to supported polar strap device
+    func autoConnect() {
+        autoConnectDisposable?.dispose()
+        autoConnectDisposable = api.startAutoConnectToDevice(-55,service:nil,
+                    polarDeviceType: nil)
+            .subscribe{ e in
+                       switch e {
+                        case .completed:
+                            NSLog("auto connect search complete")
+                        case .error(let err):
+                            NSLog("auto connect failed: \(err)")
+                        }
+            }
+    }
+    
+    
+}
+
+extension PolarBleSdkManager {
+    enum ConnectionState {//"Define an enumeration type ConnectionState which
+                        // can take either a value of disconnected with nothing
+                        // or a value of connection/connected with a value of String
+        case disconnected
+        case connection(String)
+        case connected(String)
+    }
+}
--- PSHR_iOS/Views/ContentView.swift
+++ PSHR_iOS/Views/ContentView.swift
@@ -6,16 +6,81 @@
 //
 
 import SwiftUI
+import PolarBleSdk
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+extension Text {
+    func headerStyle() -> some View {
+        self
+            .font(.headline)
+            .foregroundColor(.secondary)
+            .fontWeight(.light)
     }
 }
 
+struct ContentView: View {
+    @ObservedObject var bleSdkManager: PolarBleSdkManager
+    
+    // What is actually displayed by the app
+    var body: some View {
+        VStack {
+            // Title at the top of the app
+            Text("PSHR BLE App")
+                .bold
+            
+            ScrollView(.vertical){
+                VStack(spacing: 10) {
+                    if !bleSdkManager.isBluetoothOn {
+                        Text("Bluetooth OFF")
+                            .bold()
+                            .foregroundColor(.red)
+                    }
+                    Group {
+                        Text("Connectivity:")
+                            .headerStyle()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        // Check what the connection status is and change the appearance of the button accordingly
+                        switch bleSdkManager.deviceConnectionState {
+                        case .disconnected:
+                            Button("Connection Status", action: {bleSdkManager.connectToDevice()})
+                                .buttonStyle(PrimaryButtonStyle(buttonState: getConnectButtonState()))
+                        case .connecting(let deviceId):
+                            Button("Connecting \(deviceId)", action: {})
+                                .buttonStyle(PrimaryButtonStyle(buttonState: getConnectButtonState()))
+                                .disabled(true)
+                        case .connected(let deviceId):
+                            Button("Disconnect \(deviceId)", action: {bleSdkManager.disconnectFromDevice()})
+                                .buttonStyle(PrimaryButtonStyle(buttonState: getConnectButtonState()))
+                        }
+                    }
+                }
+            }//ScrollView
+        }//VStack
+    }//View
+    
+    func getConnectButtonState() -> ButtonState {
+        if bleSdkManager.isBluetoothOn {
+            switch bleSdkManager.deviceConnectionState {
+            case .disconnected:
+                return ButtonState.released
+            case .connecting(_):
+                return ButtonState.disabled
+            case .connected(_):
+                return ButtonState.pressedDown
+            }
+        }
+        
+    }
+    
+}//struct
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ForEach(["iPhone 8", "iPAD Pro (12.9-inch)"], id: \.self) { deviceName in
+            ContentView(bleSdkManager: PolarBleSdkManager())
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+        
     }
 }
