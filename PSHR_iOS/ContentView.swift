044477de81cf33430b02821bed24b9ae00ab0761
Lawreros <lawreros@gmail.com>
2022-11-05 10:44:53 -0400

    Initial Commit

---
+++ PSHR_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PSHR_iOS
+//
+//  Created by Ross on 11/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PSHR_iOS/PSHR_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSHR_iOSApp.swift
+//  PSHR_iOS
+//
+//  Created by Ross on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PSHR_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
